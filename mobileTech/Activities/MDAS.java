816261f919d030c8e51bfb3bbe578eb5b23ff144
Davey <125982535+Milave-kun@users.noreply.github.com>
2023-10-01 23:01:23 +0800

    Add files via upload

---
+++ mobileTech/Activities/MDAS.java
@@ -0,0 +1,25 @@
+
+public class MDAS {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        
+        // Multiplication
+        int result1 = num1 * num2;
+        System.out.println("Multiplication result: " + result1);
+        
+        // Division
+        int result2 = num1 / num2;
+        System.out.println("Division result: " + result2);
+        
+        // Addition
+        int result3 = num1 + num2;
+        System.out.println("Addition result: " + result3);
+        
+        // Subtraction
+        int result4 = num1 - num2;
+        System.out.println("Subtraction result: " + result4);
+    }
+}
+
+                   
